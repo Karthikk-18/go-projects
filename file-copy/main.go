aa2509544af7e0829e9b124e24460612a6b2b051
Karthik Yadav <karthikgudaru72@gmail.com>
2026-08-28 23:50:21 +0530

    built a actual cp terminal cmd tool

--- file-copy/main.go
+++ file-copy/main.go
@@ -1,20 +1,51 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
 
+type progressWriter struct {
+	total int64
+}
+
 func main() {
-	file, err := os.Open("README.md")
+	filename := flag.String("file","", "usage: go run . <filename>")
+	destination := flag.String("output","", "file which stores output")
+	flag.Parse()
+	if *filename == "" {
+		log.Fatal("Please provide a file")
+	}
+	if *destination == "" {
+		log.Fatal("Please provide a destination file to store data")
+	}
+	file, err := os.Open(*filename)
 	if err != nil {
 		log.Fatal(err)
 	}
-	data := make([]byte, 1200)
-	count, err := file.Read(data)
+	defer file.Close()
+	dst, err := os.Create(*destination)
 	if err != nil {
-		log.Fatal()
+		log.Fatal(err)
 	}
-	fmt.Printf("read %d bytes: %q\n", count, data[:count])
+	defer dst.Close()
+
+	p := &progressWriter{}
+	reader := io.TeeReader(file, p)
+	
+	if _, err := io.Copy(dst,reader); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Copied %d KB..", p.total/1024)
+}
+
+func (p *progressWriter) Write(data []byte) (int, error) {
+	p.total += int64(len(data))
+	if p.total % (1024*1024) == 0 {
+		fmt.Printf("Copied %d MB... \n", p.total/(1024*1024))
+	}
+	return len(data), nil
 }
